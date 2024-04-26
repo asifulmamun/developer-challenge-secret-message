f2c3ad5c06cb9288e62dacc7198023f4d5fd201e
asifulmamun <asifulmamun@gmail.com>
2024-04-27 01:39:45 +0600

    laravel init and shwoing users in dashboard

---
+++ app/Http/Controllers/dashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class dashboardController extends Controller
+{
+
+    public function index(){
+        
+        $users = User::all();
+
+
+
+        return view('dashboard', compact('users'));
+
+    }
+
+}
